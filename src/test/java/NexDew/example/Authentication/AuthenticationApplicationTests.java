f88585c90ac49e0c35643b2aef172eec476d088c
ajay7604 <maityajay7604@gmail.com>
2025-10-30 00:41:11 -0700

    Authentication Related Changes and Implementation

---
+++ src/test/java/NexDew/example/Authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package NexDew.example.Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
